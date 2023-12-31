f5498da0a54687fe6dd42b7e2537e60c292ae125
HaroldBKim <sbookim2@gmail.com>
2023-12-31 22:49:49 +0900

    Backtracking problem04

---
+++ W05_Dec/BOJ_15650.py
@@ -0,0 +1,14 @@
+def dfs(n, s, lst):         # 멀티트리
+    if n==M:                # M개 선택 완료
+        ans.append(lst)
+        return
+
+    for j in range(s, N+1):
+        dfs(n+1, j+1, lst+[j])
+
+N, M = map(int, input().split())
+ans = []
+# dfs(1, [])    # 이진트리
+dfs(0, 1, [])   # 멀티트리
+for lst in ans:
+    print(*lst)
